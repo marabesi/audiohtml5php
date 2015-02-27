1619ff2e3f9ec20bcba64708496a642997aa8e1a
marabesi <matheus.marabesi@gmail.com>
2015-02-26 21:33:25 -0300

    Translated to English

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
 /**
- * Importante: Todos as funções referentes a javascript/jquery estão necesse
- * arquivo.
- * 
- * Ao carregar a página lista todos as músicas existentes e configura o
- * uploadfy.
+ * When the page loads the list of music is shown and the configuration
+ * of the uploadify is applied.
  * 
  * @author Matheus Marabesi
  * @see http://jquery.com/
@@ -13,50 +10,37 @@
  */
 $(document).ready(function() {
 	$('#tb_songs').dataTable({
-		'oLanguage' : {
-			// Opções para traduzir do inglês(idioma padrão do plugin) para
-			// português
-			'sProcessing' : 'Processando...',
-			'sLengthMenu' : 'Mostrar _MENU_ registros',
-			'sZeroRecords' : 'Não foram encontrados resultados',
-			'sInfo' : 'Mostrando de _START_ até _END_ de _TOTAL_ registros',
-			'sInfoEmpty' : 'Mostrando de 0 até 0 de 0 registros',
-			'sInfoFiltered' : '(filtrado de _MAX_ registros no total)',
-			'sInfoPostFix' : '',
-			'sSearch' : 'Buscar:',
-			'sUrl' : '',
-			'oPaginate' : {
-				'sFirst' : 'Primeiro',
-				'sPrevious' : 'Anterior',
-				'sNext' : 'Seguinte',
-				'sLast' : 'Último'
-			}
-		}
+		//'oLanguage' : {
+			// options to use Portuguese (PT-br)
+			//'sProcessing' : 'Processando...',
+			//'sLengthMenu' : 'Mostrar _MENU_ registros',
+			//'sZeroRecords' : 'Não foram encontrados resultados',
+			//'sInfo' : 'Mostrando de _START_ até _END_ de _TOTAL_ registros',
+			//'sInfoEmpty' : 'Mostrando de 0 até 0 de 0 registros',
+			//'sInfoFiltered' : '(filtrado de _MAX_ registros no total)',
+			//'sInfoPostFix' : '',
+			//'sSearch' : 'Buscar:',
+			//'sUrl' : '',
+			//'oPaginate' : {
+			//	'sFirst' : 'Primeiro',
+            //  'sPrevious' : 'Anterior',
+            //  'sNext' : 'Seguinte',
+            //  'sLast' : 'Último'
+			//}
+		//}
 	});
 
-	// Inicia o uploadfy
 	$('#vc_file').uploadifive({
 		'method' : 'post',
 		'swf' : '/js/uploadify/uploadify.swf',
 		'uploadScript' : '/php/controller/Uploader.php',
 		'queueID' : 'queue',
 		'multi' : false,
-		// Mudar o tamanho de acordo com o texto do botão
 		'width' : 'auto',
-		'buttonText' : 'Selecionar mp3 !',
+		'buttonText' : 'Select the mp3 !',
 		'removeCompleted' : true,
-		// Adiciona os dados faltantes ao formulário
 		'onAddQueueItem' : function() {
-			/**
-			 * O correto seria utilizar a propriedade formData como diz a
-			 * documentação (http://www.uploadify.com/), porém no dado momento
-			 * do upload não é lançado nenhum evento anterior para atualizar os
-			 * dados existeste no formulário, então sempre irá valores núlos
-			 * para o servidor para contornar isso é adicionado esse evento
-			 * (onAddQueueItem) para que toda vez que seja adicionado um arquivo
-			 * na fila seja atualizado os dados no formulário.
-			 */
-			this.data('uploadifive').settings.formData = {
+            this.data('uploadifive').settings.formData = {
 				'vc_music' : $('#vc_music').val(),
 				'lb_style' : $('#lb_style').val()
 			};
@@ -68,31 +52,28 @@ $(document).ready(function() {
 		}
 	});
 
-	// Busca os estilos de múscias disponíveis
 	updateSelect();
 
-	// Busca as múscias existentes
 	updateTable();
 });
 
 /**
- * Busca a playlist de acordo com o id passado do
- * estilo musical
+ * Search the playlist according to the last id
  */
 $('#lb_style_player').on('change', function(){
 	findPlaylist($(this).val());
 });
 
 /**
- * Para questões de alta performance essa função abaixo deve ser eliminada, e a
- * cada nova música enviada deverá ser incluida a linha na tabela
+ * For high performance issues this function below should be eliminated, and the
+ * each new song submitted must be included in the line table instead
  */
 var clearTable = function() {
 	$('#tb_songs').dataTable().fnClearTable();
 };
 
 /**
- * Busca todas as músicas cadastradas
+ * Search all the songs
  */
 var updateTable = function() {
 	$.ajax({
@@ -117,7 +98,7 @@ var updateTable = function() {
 };
 
 /**
- * Limpa todos os dados da tabela e os carrega de novo
+ * Erase all rows on the table and loads they again
  */
 var erase = function(id) {
 	$.ajax({
@@ -137,14 +118,11 @@ var erase = function(id) {
 };
 
 /**
- * Busca os estilos de múscias disponíveis e adiciona aos select as opções
- * disponíveis.
+ * Search the styles available and adds to select the options
  * 
- * #lb_style : select responsável por relacionar o estilo com a música para
- * realizar o upload
+ * #lb_style : used to select the mp3 file to be uploaded
  * 
- * #lb_style_player : select responsável por carregar todas as músicas de um
- * determinado estilo
+ * #lb_style_player : Loads all songs from a category
  */
 var updateSelect = function() {
 	$.ajax({
@@ -164,8 +142,8 @@ var updateSelect = function() {
 };
 
 /**
- * Busca todas as músicas pelo id do estilo passado
- * e as adiciona á lista de execução do player
+ * Search all songs by the id of the last style
+ * and adds them to the playlist
  */
 var findPlaylist = function(id) {
 	var ul = $('#playlist');
@@ -185,7 +163,6 @@ var findPlaylist = function(id) {
 					ul.append('<li class="playlist-item"><a href="/songs/' + data[i].vc_file +'">' + data[i].vc_music + '<a/></li>');
 				}
 				
-				// Seta configurações padrões para o player
 				player();
 			}
 		});
@@ -197,9 +174,9 @@ var findPlaylist = function(id) {
 };
 
 /**
- * Função com todas as configurações referentes ao 
- * player em HTML5, tocar, passar para a próxima música
- * e alterar as classes dos elementos.
+ * Function with all the settings related to
+ * HTML5 player, play, skip to the next song
+ * and change the classes of elements.
  */
 var player = function() {
 	var player = $('#player');
@@ -207,22 +184,21 @@ var player = function() {
 	var songs = playlist.find('li a');
 	
     var audio = player[0];
-	// Pausa caso haja alguma outra música tocando enquanto
-	// o usuário muda de playlist
+	// Pause if any other music playing while the user changes playlist
 	audio.pause();
 	
-	// Toca a primeira música da lista automaticamente
+	// Plays the first song of the list automatically
 	play(playlist.find('li a').first(), audio);
 	
 	
-	// Altera a função padrão do click no link
+	// Changes the default click behavior
 	playlist.find('li a').click(function(event){
-		// Utiliza o preventDefault para que seja anulada a ação de ir para outra página
+		// Uses preventDefault to be annulled the action of going to another page
 		event.preventDefault();
 		play($(this), audio);
 	});
 	
-	// Função para ir para a próxima música existente na linha
+	// Function to go to the next existing music on playlist
 	audio.addEventListener('ended', function(event){
 		var nextSong = $('.active').next().children();
 		play(nextSong, audio);
@@ -233,7 +209,7 @@ var player = function() {
 		player.load();
 		player.play();
 		
-		// Adiciona uma classe para saber qual som está ativo
+		// Adds a class to know what sound is active
 		$('ul li').removeClass('active');
 		song.parent().addClass('active');
 	}
